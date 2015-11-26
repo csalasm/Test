ae10ceb40a4c2041d725b4b48be19c7fd994cbc1
andresbailen93 <andresbailen93@mbp-de-andres>
2015-11-26 21:16:52 +0100

    Aqui esta todo bien de andres

--- src/controlador/ProfesorControlador.java
+++ src/controlador/ProfesorControlador.java
@@ -136,7 +136,7 @@ public class ProfesorControlador  implements ActionListener {
         vnu.tfDniUser.setText("");
         vnu.tfApellidos.setText("");
         vnu.pfPassword.setText("");
-        JOptionPane.showMessageDialog(vnu,Messages.getString(" "),null,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(vnu,Messages.getString("useradd"),null,JOptionPane.INFORMATION_MESSAGE);
         vnu.setVisible(false);
         vnu.pfPassword.setText("");
         
@@ -161,6 +161,7 @@ public class ProfesorControlador  implements ActionListener {
         vnt.jTextNombre.setText("");
         vnt.cbDuracion.setSelectedIndex(0);
         vnt.cbRestada.setSelectedIndex(0);
+        JOptionPane.showMessageDialog(vnt,Messages.getString("testadd"),null,JOptionPane.INFORMATION_MESSAGE);
         vnt.setVisible(false);
 
     }
@@ -263,7 +264,7 @@ public class ProfesorControlador  implements ActionListener {
         testdao.UpdateDispo(teste, Boolean.TRUE);
         vat.cbTesDesct.addItem(nombretestact);
         vat.cbActivaTest.removeItem(nombretestact);
-         JOptionPane.showMessageDialog(vat, "Test Activo.",null,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(vat, Messages.getString("test_acti."),null,JOptionPane.INFORMATION_MESSAGE);
     }
     private void desactivaDispo(){
         String nombretestdest=(String)vat.cbTesDesct.getSelectedItem();
@@ -271,7 +272,7 @@ public class ProfesorControlador  implements ActionListener {
         testdao.UpdateDispo(teste, Boolean.FALSE);
         vat.cbActivaTest.addItem(nombretestdest);
         vat.cbTesDesct.removeItem(nombretestdest);
-        JOptionPane.showMessageDialog(vat, "Test Desactivado.",null,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(vat, Messages.getString("test_desacti"),null,JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
